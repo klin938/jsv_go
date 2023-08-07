78bf2895016ed46f26cca7d4fe31305d8f60f5d3
Derrick Lin <d.lin@garvan.org.au>
2023-08-08 05:45:10 +1000

    added support user request h_rt/s_rt

--- garvan_hpc_sge_jsv.go
+++ garvan_hpc_sge_jsv.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"jsv"
+	"strings"
+	"time"
 )
+
+// 24:00:00/86400
+// 8:00:00/28800
+var DEFAULT_RT string = "8:00:00"
+var DEFAULT_RT_SECONDS int = 28800
+var SHORT_Q_RT string = "48:00:00"
+var SHORT_Q_RT_SECONDS int = 172800
 
 func jsvOnStartFunction() {
 	//jsv_send_env()
@@ -17,6 +27,29 @@ func jsvVerificationFunction() {
 	client, _ := jsv.GetParam("CLIENT")
 	// This param is available only in server side jsv. Empty str when in client side.
 	job_id, _ := jsv.GetParam("JOB_ID")
+
+	h_rt_val, h_rt_set := jsv.SubGetParam("l_hard", "h_rt")
+	if h_rt_set == false {
+		jsv.LogInfo("JSV: " + client + "|" + job_id + "|h_rt - set default: " + SHORT_Q_RT)
+		h_rt_val = DEFAULT_RT
+	} else {
+		// convert hh:mm:ss format into seconds
+		rt_spl := strings.Split(h_rt_val, ":")
+		rt_str := rt_spl[0] + "h" + rt_spl[1] + "m" + rt_spl[2] + "s"
+		rt, _ := time.ParseDuration(rt_str)
+		var rt_seconds int = int(rt.Seconds())
+		jsv.LogInfo("JSV: " + client + "|" + job_id + "|h_rt - user requested " + h_rt_val + "/" + fmt.Sprint(rt_seconds))
+
+		if rt_seconds > SHORT_Q_RT_SECONDS {
+			jsv.LogInfo("JSV: " + client + "|" + job_id + "|h_rt - modified: " + h_rt_val + " -> " + SHORT_Q_RT)
+			h_rt_val = SHORT_Q_RT
+		}
+	}
+	// If the user did not provide s_rt, set it to whatever h_rt is.
+	s_rt_val, s_rt_set := jsv.SubGetParam("l_hard", "s_rt")
+	if s_rt_set == false {
+		s_rt_val = h_rt_val
+	}
 	// So far this is the only way I can use to identify if a job is a batch (qsub) or
 	// interactive (qrsh, qlogin etc). I assume most people use qrsh/qlogin without a
 	// command (so to be brought to a remote compute node cmd promt). qrsh/qlogin can
@@ -24,10 +57,10 @@ func jsvVerificationFunction() {
 	// the qrsh/qlogin session is terminated automatically, which no problem for us.
 	cmdname, _ := jsv.GetParam("CMDNAME")
 	if cmdname == "NONE" {
-		jsv.LogInfo("JSV: " + client + "|" + job_id + "|" + cmdname + " - setting wall clock limits [ h_rt:8:00:00 | s_rt:8:00:00 ]")
 		// existing param is overwritten automatically
-		jsv.SubAddParam("l_hard", "h_rt", "8:00:00")
-		jsv.SubAddParam("l_hard", "s_rt", "8:00:00")
+		jsv.SubAddParam("l_hard", "h_rt", h_rt_val)
+		jsv.SubAddParam("l_hard", "s_rt", s_rt_val)
+		jsv.LogInfo("JSV: " + client + "|" + job_id + "|" + cmdname + " - setting wall clock limits [ h_rt:" + h_rt_val + " | s_rt:" + s_rt_val + " ]")
 	}
 
 	tmp_req_val, _ := jsv.SubGetParam("l_hard", "tmp_requested")
